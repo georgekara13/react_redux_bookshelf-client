1fabfa8eb4f21fa0416e550f407f71029bd41d42
georgekara13 <george_kara_13@hotmail.com>
2020-04-04 19:31:58 +0300

    Fetch book data TODO fix app crashing on load more

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,15 @@
 import axios from 'axios'
 
-export function getBooks(limit = 10, start = 0, order = 'asc'){
+export function getBooks(limit = 10, start = 0, order = 'asc', list = ''){
   const request = axios.get(`/api/getbooks?limit=${limit}&skip=${start}&order=${order}`)
-                       .then(response => response.data)
+                       .then(response => {
+                              if(list){
+                                return [...list,...response.data]
+                              }
+                              else{
+                                return response.data
+                              }
+                            })
 
   return {
     type: 'GET_BOOKS',
--- src/containers/homecontainer.js
+++ src/containers/homecontainer.js
@@ -2,21 +2,29 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { getBooks } from '../actions'
 
+import BookItem from '../widgetsUI/bookitem'
+
 class HomeContainer extends Component {
 
   componentDidMount(){
-    this.props.dispatch(getBooks())
+    this.props.dispatch(getBooks(1,0))
   }
 
   renderItems(books){
-    return books.list ? books.list.map( item => ( 'item'))
+    return books.list ? books.list.map( (item, i) => ( <BookItem {...item} key ={i}/>))
                       : null
+  }
+
+  loadMore(){
+    let count = this.props.books.list.length
+    this.props.dispatch(getBooks(1, count, 'asc', this.props.books.list))
   }
 
   render() {
     return (
       <div>
         {this.renderItems(this.props.books)}
+        <div className="loadmore" onClick={this.loadMore}>Load More</div>
       </div>
     )
   }
