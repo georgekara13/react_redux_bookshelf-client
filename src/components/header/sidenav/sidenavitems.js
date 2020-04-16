0e24277a235043fa16f458961224fdb10c9f6976
georgekara13 <george_kara_13@hotmail.com>
2020-04-17 00:54:46 +0300

    Restrict sidenav routes - TODO fix logout glitch showing all sidenav items

--- src/components/header/sidenav/sidenavitems.js
+++ src/components/header/sidenav/sidenavitems.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import FontAwesome from 'react-fontawesome'
+import { connect } from 'react-redux'
 
-const SideNavItems = () => {
+const SideNavItems = ({user}) => {
 
   const items = [
       {
@@ -17,42 +18,43 @@ const SideNavItems = () => {
         icon: 'file-text-o',
         text: 'My Profile',
         link: '/user',
-        restricted: false
+        restricted: true
       },
       {
         type: 'navItem',
         icon: 'file-text-o',
         text: 'Add Admin',
         link: '/user/register',
-        restricted: false
+        restricted: true
       },
       {
         type: 'navItem',
         icon: 'file-text-o',
         text: 'Login',
         link: '/login',
-        restricted: false
+        restricted: false,
+        exclude: true
       },
       {
         type: 'navItem',
         icon: 'file-text-o',
         text: 'My Reviews',
         link: '/user/user-posts',
-        restricted: false
+        restricted: true
       },
       {
         type: 'navItem',
         icon: 'book',
         text: 'Add Book Review',
         link: '/user/add',
-        restricted: false
+        restricted: true
       },
       {
         type: 'navItem',
         icon: 'file-text-o',
         text: 'Logout',
         link: '/user/logout',
-        restricted: false
+        restricted: true
       }
   ]
 
@@ -65,11 +67,20 @@ const SideNavItems = () => {
     </div>
   )
 
-  const showItems = () => {
-    return items.map((item, i) => {
-      return element(item, i)
-    })
-  }
+  //FIXME restriction glitches on first logout
+  const showItems = () => (
+    user.login ? items.map((item, i) => {
+                   if (user.login.isAuth){
+                     return !item.exclude ? element(item,i)
+                                          : null
+                   }
+                   else{
+                     return !item.restricted ? element(item, i)
+                                             : null
+                   }
+                 })
+               : items.map((item, i) => { return item.restricted === false ? element(item, i) : null})
+  )
 
   return (
     <div>
@@ -78,4 +89,10 @@ const SideNavItems = () => {
   )
 }
 
-export default SideNavItems
+function mapStateToProps(state){
+  return{
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(SideNavItems)
